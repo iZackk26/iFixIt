dcb8529f9bdf027fe6584d5342a37f706a42b8ee
iZackk__26 <newxstorecr@gmail.com>
2024-10-22 17:30:56 -0600

    feat: fixing comflits

--- ifixit-administrator/src/pages/Report.tsx
+++ ifixit-administrator/src/pages/Report.tsx
@@ -31,7 +31,7 @@ const Report = () => {
     try {
       const apiUrl = import.meta.env.VITE_API_KEY;
       const response = await axios.get(
-        `${apiUrl}registration/${registrationID}/details`
+        `${apiUrl}registration/${registrationID}/reports`
       );
 
       if (response) {
