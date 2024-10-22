165aa010b6fb0092641f0c911c322f08d22feeb0
iZackk__26 <newxstorecr@gmail.com>
2024-10-22 12:50:14 -0600

    feat: added Scripts update

--- Scripts/Script.sql
+++ Scripts/Script.sql
@@ -29,7 +29,13 @@ CREATE TABLE Employee (
     password VARCHAR(255)
 );
 
--- Crear la tabla Registration
+-- add workshop to employee
+ALTER TABLE Employee ADD COLUMN workshop VARCHAR(255);
+
+-- Crear el tipo ENUM para el estado de la registration
+CREATE TYPE registration_status AS ENUM ('pendiente', 'en proceso', 'completado');
+
+-- Crear la tabla Registration con las nuevas columnas
 CREATE TABLE Registration (
     id SERIAL PRIMARY KEY,
     orderNumber VARCHAR(255) NOT NULL,
@@ -37,6 +43,10 @@ CREATE TABLE Registration (
     employeeID INT,
     vehicleID INT,
     date DATE NOT NULL,
+    status registration_status NOT NULL DEFAULT 'pendiente', -- Enum para el estado
+    billing BOOLEAN NOT NULL DEFAULT FALSE, -- Si está pagado o no
+    price DECIMAL(10, 2), -- Precio asociado con la reparación
+
     CONSTRAINT fk_owner_registration FOREIGN KEY(ownerID) REFERENCES Owner(id) ON DELETE SET NULL,
     CONSTRAINT fk_employee_registration FOREIGN KEY(employeeID) REFERENCES Employee(id) ON DELETE SET NULL,
     CONSTRAINT fk_vehicle_registration FOREIGN KEY(vehicleID) REFERENCES Vehicle(id) ON DELETE SET NULL
@@ -47,3 +57,11 @@ ALTER TABLE Registration
 ADD COLUMN comments JSONB;
 
 SELECT * FROM information_schema.tables WHERE table_name = 'registration';
+
+SELECT r.orderNumber, r.status, v.brand, v.licensePlate, o.name, o.mail, o.phone
+FROM Registration r
+JOIN Vehicle v ON r.vehicleID = v.id
+JOIN Owner o ON r.ownerID = o.id
+WHERE r.status IN ('pendiente', 'en proceso');
+
+
