6170b3693dc7003a816bfe9d09e2b5af2232bfd5
iZackk__26 <newxstorecr@gmail.com>
2024-10-22 01:03:37 -0600

    feat: Added preparation for registration

--- ifixit-administrator/src/components/RegistrationSummary.tsx
+++ ifixit-administrator/src/components/RegistrationSummary.tsx
@@ -1,24 +1,46 @@
 import React from "react";
+import { getVehicle } from "../utils/vehicle";
+import { getOwner } from "../utils/owner";
+import { getUser } from "../utils/auth";
+import axios from "axios";
+
 
 export default function RegistrationSummary() {
   // Datos simulados para el resumen
-  const ownerData = {
-    name: "John Doe",
-    dni: "123456789",
-    email: "john.doe@example.com",
-    phone: "+1 555 555 5555",
-  };
+  const userData = getUser();
+  const ownerData = getOwner();
+  const vehicleData = getVehicle();
 
-  const vehicleData = {
-    brand: "Toyota",
-    year: 2021,
-    licensePlate: "ABC-1234",
-  };
+  const handleSubmit = async () => {
+
+    const apiUrl = import.meta.env.VITE_API_KEY;
+    const searchUrl = `${apiUrl}registration/`;  // La URL con la ruta POST correcta
+
+    if (!ownerData || !vehicleData || !userData) {
+      console.error("Missing data to register the vehicle");
+      return;
+    }
+    // Datos que enviarás en la solicitud
+    const registrationData = {
+      ownerID: ownerData.id, // Cambia por el ID real del propietario
+      employeeID: userData.id, // Cambia por el ID real del empleado
+      vehicleID: vehicleData.id,
+      date: new Date().toISOString(),
+      comments: {
+        comment: "Vehicle registered",
+      }
+    };
+    const response = await axios.post(searchUrl, registrationData);
+    if (response.data) {
+      console.log('Vehicle registered:', response.data);
+    }
+  }
+
 
   return (
     <div className="w-96 mx-auto p-6 space-y-6">
       <h2 className="text-2xl font-bold w-full text-center">Summary</h2>
-      
+
       {/* Owner Information */}
       <div className="p-4 border rounded space-y-2">
         <h3 className="text-xl font-semibold">Owner Information</h3>
